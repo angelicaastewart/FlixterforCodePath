6068b20c74d9a809a3042df6eedfce5625fceb66
Angelica <angelica.stewart@bison.howard.edu>
2022-09-20 08:31:55 -0400

    Initial commit

---
+++ app/src/main/java/com/cronocode/moviecatalog/services/MovieApiInterface.kt
@@ -0,0 +1,11 @@
+package com.cronocode.moviecatalog.services
+
+import com.cronocode.moviecatalog.models.MovieResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MovieApiInterface {
+
+    @GET("/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")
+    fun getMovieList(): Call<MovieResponse>
+}
